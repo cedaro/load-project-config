5dd40b9c08e7b2258eaca65d20e7523d9cc07ec3
Brady Vercher <brady@blazersix.com>
2015-02-22 20:22:10 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,63 @@
+var _ = require( 'lodash' ),
+	path = require( 'path' );
+
+function Project( grunt, config ) {
+	if ( ! ( this instanceof Project ) ) {
+        return new Project( grunt, config );
+	}
+
+	var settings = {},
+		taskMap = {};
+
+	this.settings = function( options ) {
+		options && _.merge( settings, options );
+		return settings || {};
+	}
+
+	this.taskMap = function( map ) {
+		map && _.merge( taskMap, map );
+		return taskMap || {};
+	}
+
+	this.grunt = grunt;
+	config = _.isFunction( config ) ? config() : config;
+	config.taskMap && this.taskMap( config.taskMap );
+	delete config.taskMap;
+	this.settings( config );
+}
+
+Project.prototype.getConfigPaths = function() {
+	var settings = this.settings(),
+		paths = [];
+
+	// Global config path.
+	if ( settings.paths.global.config ) {
+		paths.push( settings.paths.global.config );
+	}
+
+	// Local config path.
+	if ( settings.paths.config ) {
+		paths.push( path.join( process.cwd(), settings.paths.config ) );
+	}
+
+	return paths;
+}
+
+Project.prototype.init = function( options ) {
+	var settings = this.settings( options );
+
+	require( 'time-grunt' )( this.grunt );
+
+	require( 'load-grunt-config' )( this.grunt, {
+		configPath: this.getConfigPaths(),
+		data: settings,
+		jitGrunt: {
+			customTasksDir: settings.paths.tasks || null,
+			staticMappings: this.taskMap()
+		}
+	});
+
+	return this;
+}
+
+module.exports = Project;
