1789df742b27d6386eb8b300a3a2dd86e16e3489
Brady Vercher <brady@blazersix.com>
2015-12-12 12:45:25 -0800

    Tweak formatting.

--- index.js
+++ index.js
@@ -1,72 +1,80 @@
+/**
+ * load-project-config
+ * https://github.com/cedaro/load-project-config
+ *
+ * @copyright Copyright (c) 2015 Cedaro, LLC
+ * @license MIT
+ */
+
 'use strict';
 
-var _ = require( 'lodash' ),
-	path = require( 'path' );
+var _ = require('lodash');
+var path = require('path');
 
-function Project( grunt, config ) {
-	if ( ! ( this instanceof Project ) ) {
-		return new Project( grunt, config );
-	}
+function Project(grunt, config) {
+  if (!(this instanceof Project)) {
+    return new Project(grunt, config);
+  }
 
-	var settings = {},
-		taskMap = {};
+  var settings = {};
+  var taskMap = {};
 
-	this.settings = function( options ) {
-		options && _.merge( settings, options );
-		return settings || {};
-	};
+  this.settings = function(options) {
+    options && _.merge(settings, options);
+    return settings || {};
+  };
 
-	this.taskMap = function( map ) {
-		map && _.merge( taskMap, map );
-		return taskMap || {};
-	};
+  this.taskMap = function(map) {
+    map && _.merge(taskMap, map);
+    return taskMap || {};
+  };
 
-	this.grunt = grunt;
-	config = _.isFunction( config ) ? config() : config;
-	config.taskMap && this.taskMap( config.taskMap );
-	delete config.taskMap;
-	this.settings( config );
+  this.grunt = grunt;
+  config = _.isFunction(config) ? config() : config;
+  config.taskMap && this.taskMap(config.taskMap);
+  delete config.taskMap;
+  this.settings(config);
 }
 
 Project.prototype.getConfigPaths = function() {
-	var settings = this.settings(),
-		paths = [];
+  var settings = this.settings();
+  var paths = [];
 
-	// Global config path.
-	if ( settings.paths.global.config ) {
-		paths.push( settings.paths.global.grunt );
-	}
+  // Global config path.
+  if (settings.paths.global.config) {
+    paths.push(settings.paths.global.grunt);
+  }
 
-	// Local config path.
-	if ( settings.paths.config ) {
-		paths.push( path.join( process.cwd(), settings.paths.grunt ) );
-	}
+  // Local config path.
+  if (settings.paths.config) {
+    paths.push(path.join(process.cwd(), settings.paths.grunt));
+  }
 
-	return paths;
+  return paths;
 };
 
-Project.prototype.init = function( options ) {
-	var settings;
+Project.prototype.init = function(options) {
+  var settings;
 
-	if ( options && options.taskMap ) {
-		this.taskMap( options.taskMap );
-		delete options.taskMap;
-	}
+  if (options && options.taskMap) {
+    this.taskMap(options.taskMap);
+    delete options.taskMap;
+  }
 
-	settings = this.settings( options );
+  settings = this.settings(options);
 
-	require( 'time-grunt' )( this.grunt );
+  require('time-grunt')(this.grunt);
 
-	require( 'load-grunt-config' )( this.grunt, {
-		configPath: this.getConfigPaths(),
-		data: settings,
-		jitGrunt: {
-			customTasksDir: settings.paths.grunt + '/tasks',
-			staticMappings: this.taskMap()
-		}
-	});
+  require('load-grunt-config')(this.grunt, {
+    configPath: this.getConfigPaths(),
+    data: settings,
+    jitGrunt: {
+      customTasksDir: settings.paths.grunt + '/tasks',
+      staticMappings: this.taskMap()
+    }
+  });
 
-	return this;
+  return this;
 };
 
 module.exports = Project;
